75fb3cb7b923a5b3770726b3d92f82415a5f8d1d
H4RUTY <st22067hr@gm.ibaraki-ct.ac.jp>
2025-07-07 19:04:42 +0900

    msp mine

--- msp/MSP.c
+++ msp/MSP.c
@@ -1,54 +1,68 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <limits.h>
+
 #include "MSP.h"
 
 /* input data */
 extern unsigned num;
 extern int data[];
 
-int maxSubarray(unsigned from, unsigned num);
-
 int main() {
     printf("%d\n", maxSubarray(0, num));
 }
 
-int maxSubarray(unsigned from, unsigned num) {
-    if (num == 1) {
-        return data[from];
-    } else if (num == 2) {
-        int maxdata = data[from];
-        if (data[from + 1] > maxdata)
-            maxdata = data[from + 1];
-        if (data[from] + data[from + 1] > maxdata)
-            maxdata = data[from] + data[from + 1];
-        return maxdata;
-    } else {
-        unsigned mid = num / 2;
+int max_3(int a, int b, int c) {
+    int max = a;
+    if(b > max) max = b;
+    if(c > max) max = c;
+    return max;
+}
 
-        int max1 = maxSubarray(from, mid);
-        int max2 = maxSubarray(from + mid, num - mid);
-
-        int max3L = INT_MIN;
-        int sumL = 0;
-        for (int i = (int)(from + mid) - 1; i >= (int)from; i--) {
-            sumL += data[i];
-            if (sumL > max3L) max3L = sumL;
-        }
-
-        int max3R = INT_MIN;
-        int sumR = 0;
-        for (unsigned i = from + mid; i < from + num; i++) {
-            sumR += data[i];
-            if (sumR > max3R) max3R = sumR;
-        }
-
-        int max3 = max3L + max3R;
-
-        int max = max1;
-        if (max2 > max) max = max2;
-        if (max3 > max) max = max3;
-
-        return max;
+int max_array(int* array, int num) {
+    int max = 0;
+    for (int i = 0; i < num; i++) {
+        if (max < array[i]) max = array[i];
     }
+    return max;
+}
+
+int maxSubarray(unsigned from, unsigned num) {
+    /* stop recursive call */
+    if (num == 1) return data[from];
+    if (num == 2) return max_3(data[from], data[from+1], data[from]+data[from+1]);
+
+    /* divide */
+    unsigned mid = num / 2;
+    unsigned numA1 = mid;
+    int fromA1 = from;
+    unsigned numA2 = num - numA1;
+    int fromA2 = from + numA1;
+
+    /* conquer */
+    int max1 = maxSubarray(fromA1, numA1);
+    int max2 = maxSubarray(fromA2, numA2);
+    
+    int array3L[numA1];
+    for (int i = 0; i < numA1; i++) array3L[i] = 0;
+    for (int i = from; i < mid; i++) {
+        for (int j = i; j < from + mid; j++) {
+            array3L[i - from] += data[j];
+        }
+    }
+    int max3L = max_array(array3L, numA1);
+
+    int array3R[numA2];
+    for (int i = 0; i < numA2; i++) array3R[i] = 0;
+    for (int i = mid; i < num-1; i++) { // 2 3 4
+        for (int j = mid; j <= i; j++) {
+            array3R[num-i-1] += data[j];
+        }
+    }
+    int max3R = max_array(array3R, numA2);
+
+    int max3 = max3L + max3R;
+
+    /* combine */
+    int max = max_3(max1, max2, max3);
+    return max;
 }
